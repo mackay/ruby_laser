34b972532c8be2ed43369b809e2533545d847a7a
Doug Mackay <mackay.doug@gmail.com>
2018-04-14 18:17:06 -0700

    sweep demonstrates three cases - the active motion, and each extreme

--- sweep.py
+++ sweep.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import time
 from stepper import Stepper, STEP_SEQUENCE_TORQ
 
 PINS = [17, 27, 22, 23]
@@ -12,3 +12,16 @@ error = 0
 while True:
     error = stepper.advance(SWEEP_RANGE_DEGREES - error)
     error = stepper.reverse(SWEEP_RANGE_DEGREES - error)
+
+    error = stepper.advance(SWEEP_RANGE_DEGREES - error)
+    error = stepper.reverse(SWEEP_RANGE_DEGREES - error)
+
+    error = stepper.advance(SWEEP_RANGE_DEGREES - error)
+    error = stepper.reverse(SWEEP_RANGE_DEGREES - error)
+    time.sleep(1.0)
+
+    error = stepper.advance(SWEEP_RANGE_DEGREES - error)
+    time.sleep(1.0)
+
+    error = stepper.reverse(SWEEP_RANGE_DEGREES - error)
+    time.sleep(1.0)
