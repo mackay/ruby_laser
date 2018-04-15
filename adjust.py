c64e0f2bf14214d746674b14fa11ff60559b7dc4
Doug Mackay <mackay.doug@gmail.com>
2018-04-14 18:56:33 -0700

    added adjustment tool

--- adjust.py
+++ adjust.py
@@ -5,6 +5,6 @@ from stepper import Stepper, STEP_SEQUENCE_TORQ
 PINS = [17, 27, 22, 23]
 stepper = Stepper(PINS, STEP_SEQUENCE_TORQ)
 
-adjustment_degrees = float(sys.argv[0])
+adjustment_degrees = float(sys.argv[1])
 
 stepper.advance(adjustment_degrees)
